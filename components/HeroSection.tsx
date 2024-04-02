845bb3024a72c26488eda72a38fb9799337010c2
Elijah Ross <147133625+elijahross@users.noreply.github.com>
2024-04-02 14:07:23 +0000

    New Hero Section

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,20 +1,20 @@
-import React from 'react':
-import Image from "next";
+import React from "react";
+import Image from "next/image";
+import nextsvg from "../public/next.svg";
 
-const 
- = () => {
+const HeroSection = () => {
   return (
     <div className="flex flex-col md:flex-row">
-        <Image src={} alt="saturday" className=""/>
-        <div className="md:min-w-[60%]">
-            <h1 className="text-[36px] mb-8">
-                Implementation for Three.js <br/>
-                based on fiber and drei
-            </h1>
-            <span>Exact instructions could be found in Documentation</span> 
-        </div>
+      <Image src={nextsvg} alt="saturday" className="" />
+      <div className="md:min-w-[60%]">
+        <h1 className="text-[36px] mb-8">
+          Implementation for Three.js <br />
+          based on fiber and drei
+        </h1>
+        <span>Exact instructions could be found in Documentation</span>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default 
+export default HeroSection;
