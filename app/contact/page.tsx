2b17cc0896bc04b0bf763036645573a2511ee62f
Elijah Ross <147133625+elijahross@users.noreply.github.com>
2024-04-02 21:00:55 +0700

    created new pages

---
+++ app/contact/page.tsx
@@ -0,0 +1,13 @@
+
+import dynamic from "next/dynamic";
+
+
+export default function Modell() {
+  return (
+    <main className="min-h-screen flex-col items-center justify-center">
+      <div className="xl:w-[50%] w-full items-center font-mono flex flex-col ">
+        <h1>Some text</h1>
+      </div>
+    </main>
+  );
+}
