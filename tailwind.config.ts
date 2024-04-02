6b72da609570f4f482e6b4d92a9837b918d5d858
Elijah Ross <147133625+elijahross@users.noreply.github.com>
2024-04-02 20:17:16 +0700

    update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
 
 const config: Config = {
   content: [
@@ -13,6 +13,27 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        primary: "#f7f7f7",
+        secondary: "rgb(235, 235, 235)",
+        light: "rgb(169, 169, 169)",
+        gray: "rgb(102, 102, 102)",
+        dark: "rgb(23, 23, 23)",
+        },
+       fontFamily: {
+        mont: ["Montserrat", "sans-serif"],
+        },
+       animation: {
+        bounce: "bounce 15s infinite",
+        },
+      screens: {
+        xs: "480px",
+        ss: "620px",
+        sm: "768px",
+        md: "1060px",
+        lg: "1200px",
+        xl: "2200px",
+     },
     },
   },
   plugins: [],
