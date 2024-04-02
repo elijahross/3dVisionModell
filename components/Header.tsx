c14c79c97f73ea02f8a91eb534cb6960e8cefc66
Elijah Ross <147133625+elijahross@users.noreply.github.com>
2024-04-02 14:11:31 +0000

    Corrected

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,26 +1,36 @@
-"use client"
-import React from 'react'
-import { usePathname } from 'next/navigation';
+"use client";
+import React from "react";
+import { usePathname } from "next/navigation";
 
 const Header = () => {
-    const router = usePathname();
-    const links = [{
-        name: "Home",
-        path: "/"
-    },{ name: "Modell",
-        path: "/modell"},
-    {   name: "Contact",
-        path: "/contact"}]
+  const router = usePathname();
+  const links = [
+    {
+      name: "Home",
+      path: "/",
+    },
+    { name: "Modell", path: "/modell" },
+    { name: "Contact", path: "/contact" },
+  ];
+
   return (
     <div className="w-full h-[60px] fixed top-0 flex justify-between px-10">
-        <div className="text-[12px]"> Here is Logo..</div>
-        <div className="flex flex-row">
-            links.map(({path, name}) => { return(
-                <a key={name} href={`${path}`} className={`${router === `${path}` ? "scale-110" : "scale-100" } transition-all self-center px-4 cursor-pointer`}>{name}</a>
-            )}) 
-        </div>
+      <div className="text-[12px]"> Here is Logo..</div>
+      <div className="flex flex-row">
+        {links.map(({ path, name }) => {
+          return (
+            <a
+              key={name}
+              href={`${path}`}
+              className={`${router === `${path}` ? "scale-110" : "scale-100"} transition-all self-center px-4 cursor-pointer`}
+            >
+              {name}
+            </a>
+          );
+        })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
