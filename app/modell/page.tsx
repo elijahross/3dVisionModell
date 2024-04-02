fc45fa83f77245b42176c5734f2d8243e33e3fa6
Elijah Ross <147133625+elijahross@users.noreply.github.com>
2024-04-02 19:04:26 +0000

    new changes

--- app/modell/page.tsx
+++ app/modell/page.tsx
@@ -1,36 +1,83 @@
-'use client'
-import {Suspense, useState} from "react";
-import {Canvas} from "@react-three/fiber";
-import {PerspectiveCamera} from "@react-three/drei";
+"use client";
+import { Suspense, useState, useRef } from "react";
+import { Canvas } from "@react-three/fiber";
+import { useGLTF, PerspectiveCamera } from "@react-three/drei";
 import * as THREE from "three";
-import Model from "@/components/Model";
 
 export default function Modell() {
-const [coordinates, setCoordinates] = useState({x:"",y:""})
+  const cameraRef = useRef();
+  const modelRef = useRef();
+  const { scene, nodes, materials } = useGLTF("VisionPro.glb", true);
+  const [coordinates, setCoordinates] = useState({ x: "", y: "" });
 
-function captureCoordinates(e){
-    setCoordinates({x:`${e.clientX}`, y:`${e.xlientY}`})
+  function captureCoordinates(e) {
+    const movementArea = document.getElementById("canvas");
+    const boundingBox = movementArea.getBoundingClientRect();
 
-    document.onpointermove = captureMovement();
-    function captureMovement(){
-    try{
-    setCoordinates({x:`${e.clientX}`, y:`${e.xlientY}`})
-} catch(e){console.log(e)};
-
-}}
+    document.onpointermove = captureMovement;
+    function captureMovement(e) {
+      const { current: cam } = cameraRef;
+      let camx =
+        (-(e.clientX - boundingBox.left - boundingBox.width / 2) /
+          (boundingBox.width / 2)) *
+        2;
+      let camy =
+        (e.clientY - boundingBox.top - boundingBox.height / 2) /
+        boundingBox.height;
+      cam.position.x = 2 * camx - 1;
+      cam.position.y = camy * 8 + 1;
+      cam.lookAt(new THREE.Vector3(0, 0, 0));
+      cam.updateProjectionMatrix();
+    }
+  }
 
   return (
-    <main className="min-h-screen flex-col items-center justify-center" onMouseOver={(e) => captureCoordinates()}>
-      <div className="xl:w-[50%] w-full items-center font-mono flex flex-col ">
-        <Canvas shaodows resize={{scroll: false, offsetSize: true}}>
-            <directionalLight position={[0, 6, 6]} angle={1} penumbra={1} castShadow intensity={1.5} shadow-bias={-0.0001} />
-            <directionalLight position={[0, 1, 5]} angle={1} penumbra={1} castShadow intensity={1.2} shadow-bias={-0.0001} />
-            <PerspectiveCamera fov={75} near={0.1} far={1000} position={[0,4,5]} makeDefault manual />
-            <Suspense fallback={<p>"Loading ... "</p>}>
-                <Model crd={coordinates} />
-            </Suspense>
+    <main
+      className="min-h-screen flex-col w-full flex items-center m-auto justify-center"
+      onMouseOver={(e) => captureCoordinates()}
+    >
+      <div className="xl:w-[50%] w-[100vw] h-[100vh] items-center font-mono flex flex-col ">
+        <Canvas
+          id="canvas"
+          shaodows
+          resize={{ scroll: false, offsetSize: true }}
+        >
+          <directionalLight
+            position={[0, 6, 6]}
+            angle={1}
+            penumbra={1}
+            castShadow
+            intensity={1.5}
+            shadow-bias={-0.0001}
+          />
+          <directionalLight
+            position={[0, 1, 5]}
+            angle={1}
+            penumbra={1}
+            castShadow
+            intensity={1.2}
+            shadow-bias={-0.0001}
+          />
+          <PerspectiveCamera
+            ref={cameraRef}
+            fov={75}
+            near={0.1}
+            far={1000}
+            position={[0, 4, 5]}
+            makeDefault
+            manual
+          />
+          <Suspense fallback={<p className="text-[12px]">"Loading ... "</p>}>
+            <primitive
+              ref={modelRef}
+              object={scene}
+              position={[0, -1, 0]}
+              rotation={[0, -(Math.PI / 2), 0]}
+              scale={[10, 10, 10]}
+            />
+          </Suspense>
         </Canvas>
       </div>
     </main>
-  )
+  );
 }
--- components/Model.tsx
+++ components/Model.tsx
@@ -1,25 +1,31 @@
-"use client"
-import React, {useLayoutEffect, useRef} from 'react'
-import useGLTTF from "@react-three/drei";
+"use client";
+import React, { useLayoutEffect, useRef } from "react";
+import { useGLTF } from "@react-three/drei";
+import * as THREE from "three";
 
+const Model = ({ crd }) => {
+  const modelRef = useRef();
 
-const Model = ({crd}) => {
-    const modelRef = useRef;
+  useLayoutEffect(
+    (crd) => {
+      const { current: obj } = modelRef;
+      obj.position.x = `${crd.x}`;
+      obj.position.y = `${crd.y}`;
+    },
+    [crd],
+  );
 
-    useLayoutEffect((crd) => {
-        const {current: obj}  = modelRef;
-        obj.position.x= `${crd.x}`;
-        obj.position.y= `${crd.y}`;
-
-        
-    }, [crd])
-
-    const {scene, nodes, materials} = useGLTF("VisionPro.glb", true)
+  const { scene, nodes, materials } = useGLTF("VisionPro.glb", true);
   return (
     <div>
-        <primitive ref={modelRef} object={scene} position={[0,3,0]} scale={[20,30,20]}/>
+      <primitive
+        ref={modelRef}
+        object={scene}
+        position={[0, 3, 0]}
+        scale={[20, 30, 20]}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Model
+export default Model;
