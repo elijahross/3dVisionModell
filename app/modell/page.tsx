46d5042c500ceb94cb08d9b830bf7b7447feb088
Elijah Ross <147133625+elijahross@users.noreply.github.com>
2024-04-02 22:08:43 +0700

    Outsourced Modell

--- app/modell/page.tsx
+++ app/modell/page.tsx
@@ -1,16 +1,16 @@
 'use client'
 import {Suspense, useState} from "react";
 import {Canvas} from "@react-three/fiber";
-import {PerspectiveCamera, useGLTF} from "@react-three/drei";
+import {PerspectiveCamera} from "@react-three/drei";
 import * as THREE from "three";
+import Model from "@/components/Model";
 
 export default function Modell() {
-const {scene, nodes, materials} = useGLTF("VisionPro.glb", true)
 const [coordinates, setCoordinates] = useState({x:"",y:""})
 
 function captureCoordinates(e){
     setCoordinates({x:`${e.clientX}`, y:`${e.xlientY}`})
-    
+
     document.onpointermove = captureMovement();
     function captureMovement(){
     try{
@@ -27,7 +27,7 @@ function captureCoordinates(e){
             <directionalLight position={[0, 1, 5]} angle={1} penumbra={1} castShadow intensity={1.2} shadow-bias={-0.0001} />
             <PerspectiveCamera fov={75} near={0.1} far={1000} position={[0,4,5]} makeDefault manual />
             <Suspense fallback={<p>"Loading ... "</p>}>
-            <primitive object={scene} position={[0,3,0]} scale={[20,30,20]}/>
+                <Model crd={coordinates} />
             </Suspense>
         </Canvas>
       </div>
--- components/Model.tsx
+++ components/Model.tsx
@@ -1,8 +1,24 @@
-import React from 'react'
+"use client"
+import React, {useLayoutEffect, useRef} from 'react'
+import useGLTTF from "@react-three/drei";
 
-const Model = () => {
+
+const Model = ({crd}) => {
+    const modelRef = useRef;
+
+    useLayoutEffect((crd) => {
+        const {current: obj}  = modelRef;
+        obj.position.x= `${crd.x}`;
+        obj.position.y= `${crd.y}`;
+
+        
+    }, [crd])
+
+    const {scene, nodes, materials} = useGLTF("VisionPro.glb", true)
   return (
-    <div>Model</div>
+    <div>
+        <primitive ref={modelRef} object={scene} position={[0,3,0]} scale={[20,30,20]}/>
+    </div>
   )
 }
 
