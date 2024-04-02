334048635b33031e3846b14eca40e2c8672f835e
Elijah Ross <147133625+elijahross@users.noreply.github.com>
2024-04-02 21:56:59 +0700

    Added Footer

---
+++ components/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="w-full flex flex-row px-32 min-h-[60px] items-center justify-between border-t-2 border-gray">
+        <div className="text-[12px]"> Logo goes here...</div>
+        <div className="text-[10px]"> Links Sektion</div>
+        <div className="text-[10px]"> Links Sektion</div>
+        <div className="text-[10px]"> Links Sektion</div>
+    </div>
+  )
+}
+
+export default Footer
