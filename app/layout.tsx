3ecbbc9762422417d7e84c4bf84b498374cab860
Elijah Ross <147133625+elijahross@users.noreply.github.com>
2024-04-03 02:49:39 +0000

    changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,8 +20,8 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Header />
-        {children}
-        <Footer/>
+        <div className="py-32">{children}</div>
+        <Footer />
       </body>
     </html>
   );
